7d909b3183b86f7f80754f2cb9127f05237f6b14
Xiao Hanyu <xiaohanyu1988@gmail.com>
2014-11-18 14:08:28 +0800

    Update zsh settings for ubuntu.

--- roles/base/files/zsh_profile.osx.sh
+++
@@ -1,4 +0,0 @@
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-
-export EDITOR=vim
---
+++ roles/base/files/zsh_profile.sh
@@ -0,0 +1,6 @@
+# http://stackoverflow.com/questions/7165108/in-osx-lion-lang-is-not-set-to-utf8-how-fix
+# prefer utf-8 as locale
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+export EDITOR=vim
