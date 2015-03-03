0c6a6ae1c9a563e487438b564e8d4842f0bef03c
Xiao <xiaohanyu1988@gmail.com>
2015-03-03 21:54:42 +0800

    Enable homebrew plugin in oh-my-zsh oh Mac.
    Ref: https://github.com/robbyrussell/oh-my-zsh/wiki/Plugins#brew

--- roles/basic/templates/zshrc.sh
+++ roles/basic/templates/zshrc.sh
@@ -1,6 +1,9 @@
 # Path to your oh-my-zsh installation.
 export ZSH=$HOME/.oh-my-zsh
 
+# determine OS/platform
+unamestr=$(uname)
+
 # Set name of the theme to load.
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
@@ -45,7 +48,12 @@ ZSH_THEME="{{zsh_theme}}"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git)
+
+if [[ $unamestr == "Darwin" ]]; then
+    plugins=(git brew)
+else
+    plugins=(git)
+fi
 
 source $ZSH/oh-my-zsh.sh
 
