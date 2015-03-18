899ff4372eb86cb60fa22b43aba0fa1f6107b2cc
Xiao Hanyu <xiaohanyu1988@gmail.com>
2015-03-18 15:50:49 +0800

    Remove duplicate code.
    Link `$HOME/.profile` to `$HOME/.zprofile`, thus make bash and zsh share
    the same rc file.

--- roles/basic/files/zsh_profile.sh
+++ roles/basic/files/zsh_profile.sh
@@ -9,5 +9,18 @@ export EDITOR=vim
 alias pyhttp="python -m SimpleHTTPServer"
 
 # ruby bundler
-alias b="bundle "
-alias be="bundle exec "
+if [[ -f $(which bundle) ]]; then
+    alias b="bundle "
+    alias be="bundle exec "
+fi
+
+# $ brew cask install mactex
+# ==> Caveats
+# To use mactex, zsh users may need to add the following line to their
+# ~/.zprofile.  (Among other effects, /usr/texbin will be added to the
+# PATH environment variable):
+
+#   eval `/usr/libexec/path_helper -s`
+if [[ -f /usr/libexec/path_helper ]]; then
+    eval `/usr/libexec/path_helper -s`
+fi
