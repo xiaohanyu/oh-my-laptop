40592dde1394ef41e8da3aefe1a92e5741630940
Xiao Hanyu <xiaohanyu1988@gmail.com>
2015-06-19 12:50:42 +0800

    Provide an empty `PATH` variable for `path_helper`.

--- roles/basic/files/zsh_profile.sh
+++ roles/basic/files/zsh_profile.sh
@@ -13,14 +13,3 @@ if [[ -f $(which bundle) ]]; then
     alias b="bundle "
     alias be="bundle exec "
 fi
-
-# $ brew cask install mactex
-# ==> Caveats
-# To use mactex, zsh users may need to add the following line to their
-# ~/.zprofile.  (Among other effects, /usr/texbin will be added to the
-# PATH environment variable):
-
-#   eval `/usr/libexec/path_helper -s`
-if [[ -f /usr/libexec/path_helper ]]; then
-    eval `/usr/libexec/path_helper -s`
-fi
