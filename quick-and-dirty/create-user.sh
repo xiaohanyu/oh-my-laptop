c21990a5a7d7061db4a0a9312fd8ae6525c17e46
Xiao Hanyu <xiaohanyu1988@gmail.com>
2016-03-23 22:05:47 +0800

    Enable password-less sudo priviledge for newly-created user.

--- quick-and-dirty/create-user.sh
+++ quick-and-dirty/create-user.sh
@@ -9,10 +9,11 @@ elif [[ $unamestr == "Linux"  && -f $(which apt-get) ]]; then
     apt-get install -y fish
 fi
 
-
 user=$1
 group=$2
 
 groupadd $2
 useradd -b /home -m -s `which fish` -g $group -G sudo $user
 chown -R $user:$group /home/$user
+
+echo "$user ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$user
