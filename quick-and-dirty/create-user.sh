254c297e56e1317f2a41768b659f17e21ad7af10
Xiao Hanyu <xiaohanyu1988@gmail.com>
2017-06-25 17:05:01 +0800

    Replace fish with zsh

--- quick-and-dirty/create-user.sh
+++ quick-and-dirty/create-user.sh
@@ -4,16 +4,16 @@
 unamestr=$(uname)
 
 if [[ $unamestr == "Darwin" ]]; then
-    brew install fish;
+    brew install zsh;
 elif [[ $unamestr == "Linux"  && -f $(which apt-get) ]]; then
-    apt-get install -y fish
+    apt-get install -y zsh
 fi
 
 user=$1
 group=$2
 
 groupadd $2
-useradd -b /home -m -s `which fish` -g $group -G sudo $user
+useradd -b /home -m -s `which zsh` -g $group -G sudo $user
 chown -R $user:$group /home/$user
 
 echo "$user ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$user
