7673e34410a04d22c74698088ed78193ace27e9e
Xiao Hanyu <xiaohanyu1988@gmail.com>
2017-06-26 02:06:18 +0800

    Install latest ansible via apt-get.

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,6 +10,9 @@ if [[ $unamestr == "Darwin" ]]; then
     brew install ansible
 # debian, ubuntu, mint etc.
 elif [[ $unamestr == "Linux"  && -f $(which apt-get) ]]; then
+    sudo apt-get install software-properties-common
+    sudo apt-add-repository ppa:ansible/ansible
+    sudo apt-key adv --keyserver pgp.mit.edu --recv-keys A4A9406876FCBD3C456770C88C718D3B5072E1F5
     sudo apt-get update --yes
     sudo apt-get install --yes python-pip python-dev git ansible
 fi
