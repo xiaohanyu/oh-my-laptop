32ff5f3bbf4bfe9619a2b5a34d4e87d362aee1ab
Xiao Hanyu <xiaohanyu1988@gmail.com>
2017-06-26 01:24:29 +0800

    Get ansible via system's package manager.

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,12 +7,10 @@ if [[ $unamestr == "Darwin" ]]; then
     pip || easy_install pip
     brew || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     brew cask || brew install caskroom/cask/brew-cask
+    brew install ansible
 # debian, ubuntu, mint etc.
 elif [[ $unamestr == "Linux"  && -f $(which apt-get) ]]; then
     sudo apt-get update --yes
-    sudo apt-key adv --keyserver pgp.mit.edu --recv-keys A4A9406876FCBD3C456770C88C718D3B5072E1F5
-    sudo apt-get install --yes python-dev git
-    sudo apt-get install --yes python-pip
+    sudo apt-get install --yes python-pip python-dev git ansible
+    sudo apt-get install --yes
 fi
-
-sudo pip install ansible
