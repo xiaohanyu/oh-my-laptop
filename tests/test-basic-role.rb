4933a16ab33bec93995346018397954506ebbafa
Xiao Hanyu <xiaohanyu1988@gmail.com>
2017-06-26 18:52:13 +0800

    [#3] Add GNU global.

--- tests/test-basic-role.rb
+++ tests/test-basic-role.rb
@@ -3,7 +3,8 @@ require 'base-test.rb'
 
 class BasicRoleTest < BaseTest
   def setup
-    @command_list = ['ag', 'cloc', 'editorconfig', 'htop', 'md5sum', 'mosh',
-                     'ranger', 'rlwrap', 'wget', 'tmux', 'zsh']
+    @command_list = ['ag', 'cloc', 'editorconfig', 'global', 'gtags', 'globash',
+                     'htop', 'md5sum', 'mosh', 'ranger', 'rlwrap', 'wget',
+                     'tmux', 'zsh']
   end
 end
