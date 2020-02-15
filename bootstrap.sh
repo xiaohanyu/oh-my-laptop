dec6554fb5a1a9306ae9e054c6630030bdd88fc0
Hanyu Xiao <hanyu@Hanyus-iMac.local>
2020-02-15 16:19:35 +0800

    No need to trigger `brew tap` manually

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,6 @@ unamestr=$(uname)
 if [[ $unamestr == "Darwin" ]]; then
     pip || easy_install pip
     brew || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    brew cask || brew tap caskroom/cask
+    brew cask
     brew install ansible
 fi
