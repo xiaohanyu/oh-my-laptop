01e6291c62738e6b927c6fa9b815bae970b73110
Xiao Hanyu <xiaohanyu1988@gmail.com>
2022-12-07 10:13:15 +0800

    fix autojump init file on arm64 based mac

--- roles/basic/files/autojump-init.sh
+++ roles/basic/files/autojump-init.sh
@@ -1,4 +1,11 @@
 # macOS
+
+# for m1/m2 arm based mac
+if [ -f /opt/homebrew/etc/profile.d/autojump.sh ]; then
+    . /opt/homebrew/etc/profile.d/autojump.sh
+fi
+
+# for intel based mac
 if [ -f /usr/local/etc/profile.d/autojump.sh ]; then
     . /usr/local/etc/profile.d/autojump.sh
 fi
